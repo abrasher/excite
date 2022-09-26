b8be4717c0b99b4c68849efc12b33b7a3fd0f309
Amber Brasher <abrasher@urbantech.com>
2022-09-26 10:36:13 -0400

    removed commands and added excel custom functions

---
+++ functions/functions.ts
@@ -0,0 +1,9 @@
+/**
+ * test function
+ * @customfunction
+ */
+function test(): string {
+  return "hi"
+}
+
+CustomFunctions.associate("TEST", test)
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,21 @@ import vue from "@vitejs/plugin-vue"
 import { readFileSync } from "fs"
 import { homedir } from "os"
 import { resolve } from "path"
+import customFunctionsMetadataPlugin from "vite-plugin-custom-functions-metadata"
 
 const homeDir = homedir()
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    customFunctionsMetadataPlugin({
+      inputFile: resolve(__dirname, "functions/functions.ts"),
+      outputFile: resolve(__dirname, "functions/functions.json"),
+    }),
+  ],
   server: {
+    port: 3000,
+    strictPort: true,
     https: {
       key: readFileSync(resolve(`${homedir}/.office-addin-dev-certs/localhost.key`)),
       cert: readFileSync(resolve(`${homedir}/.office-addin-dev-certs/localhost.crt`)),
